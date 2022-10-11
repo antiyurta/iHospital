b1e2155a528a02b67096ccbf676fff32142c0f35
EnkhturBaina <eegii.111.gg@gmail.com>
2022-10-11 13:37:03 +0800

    zaswar

--- src/components/pages/EMR/GeneralInspection.js
+++ src/components/pages/EMR/GeneralInspection.js
@@ -1,48 +1,108 @@
 //EMR -> Явцын үзлэг -> Ерөнхий үзлэг
-import React from "react";
-import { Col, Radio, Row, Divider, Typography, Input, Button } from "antd";
+import React, { useState } from "react";
+import {
+  Col,
+  Radio,
+  Row,
+  Divider,
+  Typography,
+  Input,
+  Button,
+  Form,
+} from "antd";
 import { INPUT_HEIGHT } from "../../../constant";
 import { blue } from "@ant-design/colors";
 
 export default function GeneralInspection(props) {
   const { TextArea } = Input;
   const { Text } = Typography;
+  const [validStep2, setValidStep2] = useState(false);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    setValidStep2(true);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    setValidStep2(false);
+  };
   return (
-    <div>
+    <Form
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      labelAlign="left"
+      scrollToFirstError
+    >
       <Row align="middle" className="mb-1">
-        <Col span={6} className="text-left">
-          <Text>Биеийн ерөнхий байдал</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Дунд</Radio>
-            <Radio value={2}>Хүндэвтэр</Radio>
-            <Radio value={3}>Хүнд</Radio>
-            <Radio value={4}>Маш хүнд</Radio>
-          </Radio.Group>
+        <Col span={24} className="text-left">
+          <Form.Item
+            label="Биеийн ерөнхий байдал"
+            name="val1"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 2,
+              span: 12,
+            }}
+            labelCol={{
+              span: 6,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Дунд</Radio>
+              <Radio value={2}>Хүндэвтэр</Radio>
+              <Radio value={3}>Хүнд</Radio>
+              <Radio value={4}>Маш хүнд</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="mb-1">
-        <Col span={6} className="text-left">
-          <Text>Ухаан санаа</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Саруул</Radio>
-            <Radio value={2}>Бүдгэрсэн</Radio>
-            <Radio value={3}>Ухаангүй</Radio>
-          </Radio.Group>
+        <Col span={24} className="text-left">
+          <Form.Item
+            label="Ухаан санаа"
+            name="val2"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 2,
+              span: 12,
+            }}
+            labelCol={{
+              span: 6,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Саруул</Radio>
+              <Radio value={2}>Бүдгэрсэн</Radio>
+              <Radio value={3}>Ухаангүй</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="mb-1">
-        <Col span={6} className="text-left">
-          <Text>Арьс салст</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Хэвийн</Radio>
-            <Radio value={2}>Хэвийн бус</Radio>
-          </Radio.Group>
+        <Col span={24} className="text-left">
+          <Form.Item
+            label="Арьс салст"
+            name="val3"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 2,
+              span: 12,
+            }}
+            labelCol={{
+              span: 6,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Хэвийн</Radio>
+              <Radio value={2}>Хэвийн бус</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
@@ -227,15 +287,23 @@ export default function GeneralInspection(props) {
         style={{ padding: 5 }}
         placeholder="Сэтгэцийн байдал"
       />
-      <Row className="mt-2">
-        <Button
-          type="primary"
-          onClick={() => console.log("SAVED TAB 2")}
-          style={{ backgroundColor: blue.primary }}
-        >
-          Хадгалах
-        </Button>
-      </Row>
-    </div>
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Row className="mt-2">
+          <Button
+            type="primary"
+            htmlType="submit"
+            onClick={() => validStep2 && console.log("SAVED TAB 2")}
+            style={{ backgroundColor: blue.primary }}
+          >
+            Хадгалах
+          </Button>
+        </Row>
+      </Form.Item>
+    </Form>
   );
 }
--- src/components/pages/EMR/patientHistory/Step1.js
+++ src/components/pages/EMR/patientHistory/Step1.js
@@ -1,5 +1,5 @@
 //EMR -> Явцын үзлэг -> Өвчтөний түүх -> Төрөлт, өсөлт бойжилт
-import React from "react";
+import React, { useState } from "react";
 import {
   Col,
   Radio,
@@ -8,15 +8,15 @@ import {
   Input,
   DatePicker,
   InputNumber,
-  Typography,
   Button,
+  Form,
 } from "antd";
 import { INPUT_HEIGHT } from "../../../../constant";
 import { blue } from "@ant-design/colors";
 
 export default function Step1(props) {
   const { TextArea } = Input;
-  const { Text } = Typography;
+  const [validStep1, setValidStep1] = useState(false);
 
   const onChangeBirthDate = (date, dateString) => {
     console.log(date, dateString);
@@ -24,128 +24,230 @@ export default function Step1(props) {
   const onChangeWeek = (value) => {
     console.log("changed", value);
   };
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    setValidStep1(true);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    setValidStep1(false);
+  };
   return (
-    <div>
+    <Form
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      labelAlign="left"
+      scrollToFirstError
+    >
       <Divider orientation="left" className="text-sm my-2">
         Төрөлт
       </Divider>
       <Row align="middle" className="mb-1">
-        <Col span={4} className="text-left">
-          <Text>Хэдэн онд</Text>
-        </Col>
-        <Col span={8}>
-          <DatePicker
-            onChange={onChangeBirthDate}
-            placeholder="1999-01-01"
-            style={{
-              minHeight: INPUT_HEIGHT,
-              padding: 5,
+        <Col span={12}>
+          <Form.Item
+            label="Хэдэн онд"
+            name="birthDate"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 2,
+              span: 12,
             }}
-          />
+          >
+            <DatePicker
+              onChange={onChangeBirthDate}
+              placeholder="1999-01-01"
+              style={{
+                minHeight: INPUT_HEIGHT,
+                padding: 5,
+              }}
+            />
+          </Form.Item>
         </Col>
-        <Col span={6} className="text-center">
-          <Text>Хаана</Text>
-        </Col>
-        <Col span={6}>
-          <Input
-            size="small"
-            style={{
-              minHeight: INPUT_HEIGHT,
-              padding: 5,
-              height: INPUT_HEIGHT,
+        <Col span={12}>
+          <Form.Item
+            label="Хаана"
+            name="location"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 2,
+              span: 12,
             }}
-          />
+          >
+            <Input
+              size="small"
+              style={{
+                minHeight: INPUT_HEIGHT,
+                padding: 5,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="my-2">
-        <Col span={4} className="text-left">
-          <Text>Яаж төрсөн</Text>
-        </Col>
-        <Col span={20}>
-          <Radio.Group>
-            <Radio value={1}>Төрөх замаар</Radio>
-            <Radio value={2}>Кесар хагалгаагаар</Radio>
-          </Radio.Group>
+        <Col span={16}>
+          <Form.Item
+            label="Яаж төрсөн"
+            name="born"
+            rules={[{ required: true, message: "Заавал сонгоно уу" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Төрөх замаар</Radio>
+              <Radio value={2}>Кесар хагалгаагаар</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="mb-1">
-        <Col span={4} className="text-left">
-          <Text>Хугацаандаа</Text>
+        <Col span={12} className="text-left">
+          <Form.Item
+            label="Хугацаандаа"
+            name="onTime"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+          >
+            <DatePicker
+              onChange={onChangeBirthDate}
+              placeholder="1999-01-01"
+              style={{
+                minHeight: INPUT_HEIGHT,
+                padding: 5,
+              }}
+            />
+          </Form.Item>
         </Col>
         <Col span={8}>
-          <DatePicker
-            onChange={onChangeBirthDate}
-            placeholder="1999-01-01"
-            style={{
-              minHeight: INPUT_HEIGHT,
-              padding: 5,
+          <Form.Item
+            label="Хэдэн долоо хоног"
+            name="weeks"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
             }}
-          />
-        </Col>
-        <Col span={6} className="text-center">
-          <Text>Хэдэн долоо хоног</Text>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            min={1}
-            max={10}
-            defaultValue={1}
-            onChange={onChangeWeek}
-            style={{
-              minHeight: INPUT_HEIGHT,
-              height: INPUT_HEIGHT,
-            }}
-          />
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              onChange={onChangeWeek}
+              style={{
+                minHeight: INPUT_HEIGHT,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
         Бага насны өсөлт
       </Divider>
       <Row align="middle" className="my-2">
-        <Col span={8} className="text-left">
-          <Text>Бага насны өсөлт, бойжилт</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Хэвийн</Radio>
-            <Radio value={2}>Хэвийн бус</Radio>
-          </Radio.Group>
+        <Col span={16} className="text-left">
+          <Form.Item
+            label="Бага насны өсөлт, бойжилт"
+            name="growth"
+            rules={[{ required: true, message: "Заавал сонгоно уу" }]}
+            className="mb-0"
+            labelAlign="left"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+            labelCol={{
+              span: 10,
+            }}
+          >
+            <Radio.Group
+            // style={{ borderColor: "red", borderWidth: 1, borderRadius: 2 }}
+            >
+              <Radio value={1}>Хэвийн</Radio>
+              <Radio value={2}>Хэвийн бус</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="my-2">
-        <Col span={8} className="text-left">
-          <Text>Цэцэрлэгт явсан эсэх</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Тийм</Radio>
-            <Radio value={2}>Үгүй</Radio>
-          </Radio.Group>
+        <Col span={16} className="text-left">
+          <Form.Item
+            label="Цэцэрлэгт явсан эсэх"
+            name="kinder"
+            rules={[{ required: true, message: "Заавал сонгоно уу" }]}
+            className="mb-0"
+            labelAlign="left"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+            labelCol={{
+              span: 10,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Тийм</Radio>
+              <Radio value={2}>Үгүй</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Row align="middle" className="my-2">
-        <Col span={8} className="text-left">
-          <Text>Сургуульд сурсан эсэх</Text>
-        </Col>
-        <Col span={16}>
-          <Radio.Group>
-            <Radio value={1}>Тийм</Radio>
-            <Radio value={2}>Үгүй</Radio>
-          </Radio.Group>
+        <Col span={16} className="text-left">
+          <Form.Item
+            label="Сургуульд сурсан эсэх"
+            name="school"
+            rules={[{ required: true, message: "Заавал сонгоно уу" }]}
+            className="mb-0"
+            labelAlign="left"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+            labelCol={{
+              span: 10,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Тийм</Radio>
+              <Radio value={2}>Үгүй</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
         Дархлаажуулалт
       </Divider>
       <Row align="middle" className="my-2">
-        <Col span={12} className="text-left">
-          <Text>Товлолын дагуу вакцинууддаа хамрагдсан эсэх</Text>
-        </Col>
-        <Col span={12}>
-          <Radio.Group>
-            <Radio value={1}>Тийм</Radio>
-            <Radio value={2}>Үгүй</Radio>
-          </Radio.Group>
+        <Col span={20} className="text-left">
+          <Form.Item
+            label="Товлолын дагуу вакцинууддаа хамрагдсан эсэх"
+            name="vaccine"
+            rules={[{ required: true, message: "Заавал сонгоно уу" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 20,
+            }}
+          >
+            <Radio.Group>
+              <Radio value={1}>Хэвийн</Radio>
+              <Radio value={2}>Хэвийн бус</Radio>
+            </Radio.Group>
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
@@ -153,88 +255,145 @@ export default function Step1(props) {
       </Divider>
       <Row align="middle" className="my-2">
         <Col span={24} className="text-left">
-          <TextArea rows={2} style={{ padding: 2 }} />
+          <Form.Item
+            label=""
+            name="child"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+          >
+            <TextArea rows={2} style={{ padding: 2 }} />
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
         Биеийн хэмжигдэхүүн
       </Divider>
       <Row align="middle" className="mb-1">
-        <Col span={6} className="text-left">
-          <Text>Биеийн өндөр (см)</Text>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            min={1}
-            max={10}
-            defaultValue={1}
-            onChange={onChangeWeek}
-            style={{
-              minHeight: INPUT_HEIGHT,
-              height: INPUT_HEIGHT,
+        <Col span={12} className="text-left">
+          <Form.Item
+            label="Биеийн өндөр (см)"
+            name="height"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 12,
             }}
-          />
-        </Col>
-        <Col span={6} className="text-center">
-          <Text>Биеийн жин (кг)</Text>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            min={1}
-            max={10}
-            defaultValue={1}
-            onChange={onChangeWeek}
-            style={{
-              minHeight: INPUT_HEIGHT,
-              height: INPUT_HEIGHT,
+            labelCol={{
+              span: 10,
             }}
-          />
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              onChange={onChangeWeek}
+              style={{
+                minHeight: INPUT_HEIGHT,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12} className="text-center">
+          <Form.Item
+            label="Биеийн жин (кг)"
+            name="weight"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 12,
+            }}
+            labelCol={{
+              span: 10,
+            }}
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              onChange={onChangeWeek}
+              style={{
+                minHeight: INPUT_HEIGHT,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
         </Col>
       </Row>
       <Divider orientation="left" className="text-sm my-2">
         БЖИ
       </Divider>
       <Row align="middle" className="mb-1">
-        <Col span={6} className="text-left">
-          <Text>Систол даралт</Text>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            min={1}
-            max={10}
-            defaultValue={1}
-            onChange={onChangeWeek}
-            style={{
-              minHeight: INPUT_HEIGHT,
-              height: INPUT_HEIGHT,
+        <Col span={12} className="text-center">
+          <Form.Item
+            label="Систол даралт"
+            name="daralt1"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 12,
             }}
-          />
-        </Col>
-        <Col span={6} className="text-center">
-          <Text>Диастол даралт</Text>
-        </Col>
-        <Col span={6}>
-          <InputNumber
-            min={1}
-            max={10}
-            defaultValue={1}
-            onChange={onChangeWeek}
-            style={{
-              minHeight: INPUT_HEIGHT,
-              height: INPUT_HEIGHT,
+            labelCol={{
+              span: 10,
             }}
-          />
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              onChange={onChangeWeek}
+              style={{
+                minHeight: INPUT_HEIGHT,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12} className="text-center">
+          <Form.Item
+            label="Диастол даралт"
+            name="daralt2"
+            rules={[{ required: true, message: "" }]}
+            className="mb-0"
+            wrapperCol={{
+              offset: 1,
+              span: 12,
+            }}
+            labelCol={{
+              span: 10,
+            }}
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              onChange={onChangeWeek}
+              style={{
+                minHeight: INPUT_HEIGHT,
+                height: INPUT_HEIGHT,
+              }}
+            />
+          </Form.Item>
         </Col>
       </Row>
       <Row className="mt-2">
-        <Button
-          type="primary"
-          onClick={() => props.nextBtn()}
-          style={{ backgroundColor: blue.primary }}
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
         >
-          Дараах
-        </Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            onClick={() => {
+              validStep1 && props.nextBtn();
+            }}
+            style={{ backgroundColor: blue.primary }}
+          >
+            Дараах
+          </Button>
+        </Form.Item>
       </Row>
-    </div>
+    </Form>
   );
 }
