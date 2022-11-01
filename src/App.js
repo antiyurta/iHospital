9bab43642533f633a98b228b9916ba0cfcf8c0ef
anti_yurta <amarbat.gurensoft@gmail.com>
2022-11-01 12:56:30 +0800

    123

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Room from "./components/pages/Organization/Room";
 import Bed from "./components/pages/Organization/Bed";
 //
 // import Payment from "./components/pages/Payment/Payment";
-import Payment from '../src/components/pages/Payment/Payment';
+import Payment from './components/pages/Payment/Payments';
 //
 import DoctorAppointmentSchedule from "./components/pages/Appointment/DoctorAppointmentSchedule";
 import DoctorAppointment from "./components/pages/Appointment/DoctorAppointment";
--- src/components/pages/payment/Payments.js
+++ src/components/pages/payment/Payments.js
@@ -1,8 +1,8 @@
 import { Tabs } from "antd";
 import Ambulatory from "./Ambulatory";
 
-function Payment() {
+function Payments() {
   const tabs = [{ label: "Амбултори", key: 1, children: <Ambulatory /> }];
   return <Tabs items={tabs} />;
 }
-export default Payment;
+export default Payments;
