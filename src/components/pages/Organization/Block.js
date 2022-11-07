f7fdd383272984bfc4c96afacd51e76cb5c63463
anti_yurta <amarbat.gurensoft@gmail.com>
2022-11-07 14:35:19 +0800

    block fix

--- src/components/pages/Organization/Block.js
+++ src/components/pages/Organization/Block.js
@@ -1,32 +1,18 @@
-import { Col, Row } from "antd";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectCurrentToken } from "../../../features/authReducer";
+import { Get } from "../../comman";
 import UTable from "../../UTable";
-const DEV_URL = process.env.REACT_APP_DEV_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
 function Block() {
     const token = useSelector(selectCurrentToken);
     const [roomData, setRoomData] = useState([]);
     const config = {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "x-api-key": API_KEY
-        },
-        params: {
-            page: 1,
-            limit: 5,
-        }
+        headers: {},
+        params: {}
     };
     const getFloor = async () => {
-        await axios.get(DEV_URL + 'organization/floor', config)
-            .then((response) => {
-                setRoomData(response.data.response.data);
-            })
-            .catch(() => {
-                console.log("dasd");
-            })
+        const response = await Get('organization/floor', token, config);
+        setRoomData(response.data);
     }
 
     const column = [
