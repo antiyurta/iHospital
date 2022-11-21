95631bfafd66a03a5fdeda291f9e2a489952d7ac
EnkhturBaina <eegii.111.gg@gmail.com>
2022-11-22 01:40:53 +0800

    zaswar

--- src/components/pages/Bed/DashboardBed.js
+++ src/components/pages/Bed/DashboardBed.js
@@ -1,45 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row, Card } from "antd";
 import { UserAddOutlined } from "@ant-design/icons";
 import { blue } from "@ant-design/colors";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentToken } from "../../../features/authReducer";
+import { Get } from "../../comman";
 
-const cardStyle = {
-  borderColor: blue.primary,
-};
-const cardBodyStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "row",
-  padding: 12,
-  paddingRight: 10,
-  paddingLeft: 10,
-  alignItems: "center",
-  justifyContent: "space-between",
-  width: "100%",
-};
-const iconStyle = {
-  backgroundColor: blue.primary,
-  padding: 15,
-  borderRadius: 12,
-  fontSize: 20,
-  color: "#fff",
-};
-const total = {
-  fontSize: 20,
-  fontWeight: "bold",
-};
 function DashboardBed(props) {
   let navigate = useNavigate();
+  const token = useSelector(selectCurrentToken);
+  const [emptyBeds, setEmptyBeds] = useState([]); //Сул өрөө
+  const [usedBeds, setUsedBeds] = useState([]); //Дүүрсэн өрөө
+  const [repairBeds, setRepairBeds] = useState([]); //Засвартай өрөө
+
+  const config = {
+    headers: {},
+    params: {},
+  };
+  const getAllBed = async () => {
+    const response = await Get("organization/bed", token, config);
+    console.log("response get All Bed", response);
+    response.data &&
+      response.data.map((el) => {
+        if (el.status === 3) {
+          setEmptyBeds((emptyBeds) => [...emptyBeds, el]);
+        } else if (el.status === 0) {
+          setUsedBeds((usedBeds) => [...usedBeds, el]);
+        } else if (el.status === 2) {
+          setRepairBeds((repairBeds) => [...repairBeds, el]);
+        }
+      });
+  };
+  useEffect(() => {
+    getAllBed();
+  }, []);
 
   return (
     <div className="p-6">
       <Row gutter={[16, 16]}>
         <Col className="gutter-row" span={6}>
           <Card
-            style={cardStyle}
+            style={styles.cardStyle}
             className="rounded-xl cursor-pointer"
-            bodyStyle={cardBodyStyle}
+            bodyStyle={styles.cardBodyStyle}
             onClick={() => {
               props.setSelectedFn("rooms");
               navigate(`/bed_management/rooms`);
@@ -47,70 +51,46 @@ function DashboardBed(props) {
           >
             <div style={{ width: "70%" }}>
               <p>Сул орны тоо</p>
-              <p style={total}>Нийт: 7</p>
+              <p style={styles.total}>
+                Нийт: {emptyBeds !== "" && emptyBeds.length}
+              </p>
             </div>
             <div>
-              <UserAddOutlined style={iconStyle} />
+              <UserAddOutlined style={styles.iconStyle} />
             </div>
           </Card>
         </Col>
         <Col className="gutter-row" span={6}>
           <Card
-            style={cardStyle}
+            style={styles.cardStyle}
             className="rounded-xl cursor-pointer"
-            bodyStyle={cardBodyStyle}
+            bodyStyle={styles.cardBodyStyle}
           >
             <div style={{ width: "70%" }}>
               <p>Засвартай өрөө</p>
-              <p style={total}>Нийт: 5</p>
+              <p style={styles.total}>
+                Нийт: {repairBeds !== "" && repairBeds.length}
+              </p>
             </div>
             <div>
-              <UserAddOutlined style={iconStyle} />
+              <UserAddOutlined style={styles.iconStyle} />
             </div>
           </Card>
         </Col>
         <Col className="gutter-row" span={6}>
           <Card
-            style={cardStyle}
+            style={styles.cardStyle}
             className="rounded-xl cursor-pointer"
-            bodyStyle={cardBodyStyle}
-          >
-            <div style={{ width: "70%" }}>
-              <p>Нөөцөлсөн өрөө</p>
-              <p style={total}>Нийт: 3</p>
-            </div>
-            <div>
-              <UserAddOutlined style={iconStyle} />
-            </div>
-          </Card>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Card
-            style={cardStyle}
-            className="rounded-xl cursor-pointer"
-            bodyStyle={cardBodyStyle}
-          >
-            <div style={{ width: "70%" }}>
-              <p>Цэвэрлэх өрөө</p>
-              <p style={total}>Нийт: 5</p>
-            </div>
-            <div>
-              <UserAddOutlined style={iconStyle} />
-            </div>
-          </Card>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Card
-            style={cardStyle}
-            className="rounded-xl cursor-pointer"
-            bodyStyle={cardBodyStyle}
+            bodyStyle={styles.cardBodyStyle}
           >
             <div style={{ width: "70%" }}>
               <p>Дүүрсэн өрөө</p>
-              <p style={total}>Нийт: 5</p>
+              <p style={styles.total}>
+                Нийт: {usedBeds !== "" && usedBeds.length}
+              </p>
             </div>
             <div>
-              <UserAddOutlined style={iconStyle} />
+              <UserAddOutlined style={styles.iconStyle} />
             </div>
           </Card>
         </Col>
@@ -120,3 +100,30 @@ function DashboardBed(props) {
 }
 
 export default DashboardBed;
+const styles = {
+  cardStyle: {
+    borderColor: blue.primary,
+  },
+  cardBodyStyle: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    padding: 12,
+    paddingRight: 10,
+    paddingLeft: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  iconStyle: {
+    backgroundColor: blue.primary,
+    padding: 15,
+    borderRadius: 12,
+    fontSize: 20,
+    color: "#fff",
+  },
+  total: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+};
