94aff014cc3a73d521fff0c3dc914f1aa6f65e28
EnkhturBaina <eegii.111.gg@gmail.com>
2022-12-24 23:39:00 +0800

    zaswar

--- src/components/pages/Appointment/Doctor/InspectionHistory.js
+++ src/components/pages/Appointment/Doctor/InspectionHistory.js
@@ -5,74 +5,74 @@ import { useSelector } from "react-redux";
 import { selectCurrentToken } from "../../../../features/authReducer";
 import { Get, numberToCurrency, ScrollRef } from "../../../comman";
 function InspectionHistory({ patientId }) {
-    const scrollRef = useRef();
-    const token = useSelector(selectCurrentToken);
-    const [spinner, setSpinner] = useState(false);
-    const [pays, setPays] = useState([]);
-    useEffect(() => {
-        ScrollRef(scrollRef);
-    }, []);
-    const getPayments = async () => {
-        setSpinner(true);
-        const conf = {
-            headers: {},
-            params: {
-                patientId: patientId,
-                isPay: true
-            }
-        }
-        const response = await Get('payment/invoice', token, conf);
-        setPays(response.data);
-        setSpinner(false);
+  const scrollRef = useRef();
+  const token = useSelector(selectCurrentToken);
+  const [spinner, setSpinner] = useState(false);
+  const [pays, setPays] = useState([]);
+  useEffect(() => {
+    ScrollRef(scrollRef);
+  }, []);
+  const getPayments = async () => {
+    setSpinner(true);
+    const conf = {
+      headers: {},
+      params: {
+        patientId: patientId,
+        isPay: true,
+      },
     };
-    const columns = [
-        {
-            title: 'Огноо',
-            dataIndex: 'updatedAt',
-            render: (text) => {
-                return moment(text).format("YYYY-MM-DD")
-            }
-        },
-        {
-            title: "Үйлчилгээний нэр",
-            dataIndex: "name"
-        },
-        {
-            title: "Төлбөр",
-            dataIndex: "amount",
-            render: (text) => {
-                return numberToCurrency(text)
-            }
-        },
-    ];
-    useEffect(() => {
-        getPayments();
-    }, [patientId])
-    return (
-        <>
-            <Card
-                bordered={false}
-                bodyStyle={{
-                    paddingTop: 0,
-                    paddingLeft: 1,
-                    paddingRight: 1,
-                    paddingBottom: 0,
-                    maxHeight: 150,
-                    minHeight: 150,
-                    height: 150,
-                }}
-            >
-                <Table
-                    className="scroll"
-                    scroll={{
-                        y: 150
-                    }}
-                    columns={columns}
-                    dataSource={pays}
-                    pagination={null}
-                />
-            </Card>
-        </>
-    )
+    const response = await Get("payment/invoice", token, conf);
+    setPays(response.data);
+    setSpinner(false);
+  };
+  const columns = [
+    {
+      title: "Огноо",
+      dataIndex: "updatedAt",
+      render: (text) => {
+        return moment(text).format("YYYY-MM-DD");
+      },
+    },
+    {
+      title: "Үйлчилгээний нэр",
+      dataIndex: "name",
+    },
+    {
+      title: "Төлбөр",
+      dataIndex: "amount",
+      render: (text) => {
+        return numberToCurrency(text);
+      },
+    },
+  ];
+  useEffect(() => {
+    getPayments();
+  }, [patientId]);
+  return (
+    <>
+      <Card
+        bordered={false}
+        bodyStyle={{
+          paddingTop: 0,
+          paddingLeft: 1,
+          paddingRight: 1,
+          paddingBottom: 0,
+          maxHeight: 150,
+          minHeight: 150,
+          height: 150,
+        }}
+      >
+        <Table
+          className="scroll"
+          scroll={{
+            y: 150,
+          }}
+          columns={columns}
+          dataSource={pays}
+          pagination={null}
+        />
+      </Card>
+    </>
+  );
 }
 export default InspectionHistory;
