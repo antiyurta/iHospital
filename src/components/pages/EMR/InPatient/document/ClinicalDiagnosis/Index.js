5f228707844c56d0171e0152029ebde0dadea585
anti_yurta <amarbatdembee@gmail.com>
2023-01-06 12:02:58 +0800

    little fix

--- src/components/pages/EMR/InPatient/document/ClinicalDiagnosis/Index.js
+++ src/components/pages/EMR/InPatient/document/ClinicalDiagnosis/Index.js
@@ -1,8 +1,28 @@
 function Index({ form }) {
     return (
-        <div className="w-full amaraDeer amaraZuun amaraBaruun">
-            <p className="text-center font-bold">КЛИНИКИЙН УРЬДЧИЛСАН ОНОШ</p>
-        </div>
+        <>
+            <div className="w-full amaraDeer amaraZuun amaraBaruun">
+                <p className="text-center font-bold">КЛИНИКИЙН УРЬДЧИЛСАН ОНОШ</p>
+            </div>
+            <div className="w-full amaraDeer amaraZuun amaraBaruun">
+                <p className="ml-2 font-bold">Үндсэн онош:</p>
+            </div>
+            <div className="w-full amaraDeer amaraZuun amaraBaruun">
+                <p className="ml-2 font-bold">Дагадах онош:</p>
+            </div>
+            <div className="w-full amaraDeer amaraZuun amaraBaruun">
+                <p className="ml-2 font-bold">Хүндрэл:</p>
+            </div>
+            <div className="w-4/12 amaraDeer amaraZuun">
+                <p className="ml-2 font-bold">Ялган оношлох эмгэгүүд ба хам шинжүүд:</p>
+            </div>
+            <div className="w-2/12 amaraDeer amaraZuun">
+                <p className="ml-2 font-bold">Хийгдэх шинжилгээ:</p>
+            </div>
+            <div className="w-6/12 amaraDeer amaraZuun amaraBaruun">
+                <p className="ml-2 font-bold">Яаралтай хийгдэх эмчилгээ:</p>
+            </div>
+        </>
     )
 }
 export default Index;
