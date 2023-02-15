c18af6d26190aa627c0700655c56f55b93debbb1
EnkhturBaina <eegii.111.gg@gmail.com>
2023-02-15 18:05:51 +0800

    zaswar

---
+++ src/components/pages/EMR/InPatient/document/Ambulatory/AM26А.js
@@ -0,0 +1,235 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+//маягт АМ-26А
+function AM26A() {
+   const textStyle = {
+      centerText: {
+         textAlign: 'center',
+         verticalAlign: 'middle',
+         fontSize: 12,
+         padding: 0,
+         lineHeight: 1.3
+      }
+   };
+   return (
+      <div className="page-landscape">
+         <div className="subpage-landscape">
+            <div>
+               <div
+                  style={{
+                     display: 'flex',
+                     flexDirection: 'column',
+                     alignItems: 'flex-end'
+                  }}
+               >
+                  <span style={{ fontSize: 12 }}>
+                     Эрүүл мэндийн сайдын 2019 оны 12 дугаар сарын 30-ны өдрийн
+                  </span>
+                  <span style={{ fontSize: 12 }}>
+                     A/611 дүгээр тушаалын арваннэгдүгээр хавсралт
+                  </span>
+                  <span style={{ fontWeight: 'bold', fontSize: 12 }}>
+                     Эрүүл мэндийн бүртгэлийн маягт АМ-26А
+                  </span>
+               </div>
+               <div
+                  style={{
+                     display: 'flex',
+                     flexDirection: 'column'
+                  }}
+               >
+                  <span>Эмнэлгийн нэр: </span>
+               </div>
+               <div style={{ textAlign: 'center', marginBottom: 10 }}>
+                  <span style={{ fontWeight: 'bold', fontSize: 16 }}>
+                     ХӨНГӨВЧЛӨХ ЭМЧИЛГЭЭНД ХАМРАГСДЫН БҮРТГЭЛ /АМБУЛАТОРИТ/
+                  </span>
+               </div>
+            </div>
+            <Table bordered className="document">
+               <tbody>
+                  <tr>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 40 } }}
+                     >
+                        №
+                     </td>
+                     <td
+                        rowSpan={2}
+                        colSpan={10}
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ width: 100 }
+                        }}
+                     >
+                        Эцэг /эх/-ийн нэр, Нэр Регистрийн дугаар
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 60 } }}
+                     >
+                        Нас
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 60 } }}
+                     >
+                        Хүйс
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 100 } }}
+                     >
+                        Тогтмол хаяг
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 100 } }}
+                     >
+                        Онош
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 100 } }}
+                     >
+                        Гэрээр очсон огноо (с/ө/о)
+                     </td>
+                     <td colSpan={2} style={textStyle.centerText}>
+                        Тусламж үйлчилгээний төрөл
+                     </td>
+                     <td
+                        rowSpan={2}
+                        style={{ ...textStyle.centerText, ...{ width: 200 } }}
+                     >
+                        Тайлбар
+                     </td>
+                  </tr>
+                  <tr>
+                     <td style={{ ...textStyle.centerText, ...{ width: 80 } }}>
+                        Үнэгүй эм олгосон
+                     </td>
+                     <td style={{ ...textStyle.centerText, ...{ width: 80 } }}>
+                        Бусад
+                     </td>
+                  </tr>
+                  <tr style={{ height: 20 }}>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     ></td>
+                     <td
+                        colSpan={10}
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        1
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        2
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        3
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        4
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        5
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        6
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        10
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        11
+                     </td>
+                     <td
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 20 }
+                        }}
+                     >
+                        12
+                     </td>
+                  </tr>
+                  <tr>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td
+                        colSpan={10}
+                        style={{
+                           ...textStyle.centerText,
+                           ...{ height: 25 }
+                        }}
+                     ></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                     <td rowSpan={2} style={textStyle.centerText}></td>
+                  </tr>
+                  <tr>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                     <td style={{ height: 25, width: 25 }}></td>
+                  </tr>
+               </tbody>
+            </Table>
+         </div>
+      </div>
+   );
+}
+
+export default AM26A;
--- src/components/pages/EMR/InPatient/document/Ambulatory/Index.js
+++ src/components/pages/EMR/InPatient/document/Ambulatory/Index.js
@@ -4,14 +4,18 @@ import AM13A from './AM13A';
 import AM1B from './AM1B';
 import AM25A from './AM25A';
 import AM25B from './AM25B';
+import AM26B from './AM26B';
+import AM26A from './AM26А';
 
 function Index() {
    return (
       <div>
-         <AM1B />
+         {/* <AM1B /> */}
          {/* <AM13A /> */}
          {/* <AM25A /> */}
          {/* <AM25B /> */}
+         {/* <AM26A /> */}
+         <AM26B />
       </div>
    );
 }
