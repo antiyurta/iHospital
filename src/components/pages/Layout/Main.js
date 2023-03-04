2e84a6df7999f737ad19fe7a68869e7869ab6fce
EnkhturBaina <eegii.111.gg@gmail.com>
2023-03-04 20:15:37 +0800

    zaswar

--- src/components/pages/Layout/Main.js
+++ src/components/pages/Layout/Main.js
@@ -228,8 +228,9 @@ const Main = () => {
             open={isChatModal}
             placement="right"
             onClose={() => setIsChatModal(false)}
+            bodyStyle={{ padding: 0 }}
          >
-            <iframe src="http://localhost:3001" />
+            <iframe src="http://localhost:3001" width="100%" height="100%" />
          </Drawer>
       </>
    );
