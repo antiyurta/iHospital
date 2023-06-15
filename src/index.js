443763212b801ec8fa337bade2e2e9c6c603b6b4
anti_yurta <amarbatdembee@gmail.com>
2023-06-15 15:45:15 +0800

    litte fix SEO

--- src/index.js
+++ src/index.js
@@ -7,22 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import store, { Persistor } from './features/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Helmet } from 'react-helmet';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <Provider store={store}>
       <PersistGate loading={null} persistor={Persistor}>
          <BrowserRouter>
-            <Helmet>
-               <html lang="en" />
-               <title>iHospital</title>
-               <meta name="Дэлгэрэнгүй" content="Эрүүл мэндийн нэгдсэн цогц систем" />
-               <meta property="og:title" content="iHospital" />
-               <meta property="og:description" content="Эрүүл мэндийн нэгдсэн цогц систем" />
-               <meta property="og:image" content="https://www.ihospital.mn/favicon.ico" />
-               <meta property="og:url" content="https://www.ihospital.mn" />
-               <meta property="og:type" content="website" />
-            </Helmet>
             <App />
          </BrowserRouter>
       </PersistGate>
