9ad2b0a9b75412b4f74ba50dd3e67bb711e6cab7
anti_yurta <amarbatdembee@gmail.com>
2023-08-03 11:17:44 +0800

    litte fix DailyIncome

--- src/components/pages/EPayment/DailyIncome/DailyIncome.js
+++ src/components/pages/EPayment/DailyIncome/DailyIncome.js
@@ -1,25 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import { EyeOutlined, RollbackOutlined, DownloadOutlined } from '@ant-design/icons';
 import { Button, DatePicker, Modal } from 'antd';
 import moment from 'moment';
-import React, { useState, useRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { selectCurrentToken } from '../../../../features/authReducer';
-import { Get, numberToCurrency, Patch, ScrollRef } from '../../../comman';
+import { numberToCurrency, openNofi, Patch } from '../../../comman';
 import EbarimtPrint from '../EbarimtPrint';
 import mnMN from 'antd/es/calendar/locale/mn_MN';
 import PrintIndex from './PrintIndex';
 
+import PaymentServices from '../../../../services/payment/payment';
 import NewCard from '../../../Card/Card';
 import { NewColumn, NewTable } from '../../../Table/Table';
 
 const { RangePicker } = DatePicker;
 function DailyIncome() {
-   const token = useSelector(selectCurrentToken);
-   const scrollRef = useRef();
-   const config = {
-      headers: {},
-      params: {}
-   };
    const today = new Date();
    const [incomes, setIncomes] = useState([]);
    const [meta, setMeta] = useState({});
@@ -30,7 +23,6 @@ function DailyIncome() {
    const [start, setStart] = useState('');
    const [end, setEnd] = useState('');
    const [isBackPayment, setIsBackPayment] = useState(false);
-   const [totalAmount, setTotalAmount] = useState(Number);
    const [discount, setDiscount] = useState([]);
    const getDailyIncome = async (page, pageSize, start, end) => {
       setSpinner(true);
@@ -38,117 +30,43 @@ function DailyIncome() {
       end = moment(end).set({ hour: 23, minute: 59, second: 59 });
       setStart(start);
       setEnd(end);
-      const conf = {
-         headers: {},
+      await PaymentServices.getPayment({
          params: {
             page: page,
             limit: pageSize,
             startDate: moment(start).format('YYYY-MM-DD HH:mm'),
             endDate: moment(end).format('YYYY-MM-DD HH:mm')
          }
-      };
-      const response = await Get('payment/payment', token, conf);
-      setIncomes(response.data);
-      setMeta(response.meta);
-      setSpinner(false);
+      }).then((response) => {
+         setIncomes(response.data.response.data);
+         setMeta(response.data.response.meta);
+         setSpinner(false);
+      });
    };
    const getDiscount = async () => {
-      const conf = {
-         headers: {},
-         params: {}
-      };
-      const response = await Get('payment/discount', token, conf);
-      setDiscount(response.data);
+      await PaymentServices.getDiscount().then((response) => {
+         setDiscount(response.data.response.data);
+      });
    };
    const checkDiscount = (id) => {
       return discount.find((e) => e.id === id)?.name;
    };
    const viewModal = async (id, isBack) => {
       setIsBackPayment(isBack);
-      const response = await Get('payment/payment/' + id, token, config);
-      setEbarimtData(response);
-      setEbarimtModal(true);
+      await PaymentServices.getPaymentById(id).then((response) => {
+         setEbarimtData(response.data.response);
+         setEbarimtModal(true);
+      });
    };
    const reload = async (id) => {
-      const response = await Patch('payment/payment/' + id, token, config, {
-         id: id
+      await PaymentServices.patchPayment(id).then((response) => {
+         if (response.status === 200) {
+            openNofi('success', 'Амжиллтай', 'Дахин татах үйлдэл амжилттай');
+            getDailyIncome(1, 10, start, end);
+         }
       });
-      if (response === 200) {
-         getDailyIncome(1, 10, start, end);
-      }
    };
-   const incomeColumns = [
-      {
-         title: 'Овог',
-         dataIndex: ['patient', 'lastName']
-      },
-      {
-         title: 'Нэр',
-         dataIndex: ['patient', 'firstName']
-      },
-      {
-         title: 'ТТ Огноо',
-         render: (_, row) => {
-            return moment(row.createdAt).format('YYYY-MM-DD HH:mm');
-         }
-      },
-      {
-         title: 'Хөнгөлөлт',
-         dataIndex: 'discountPercentId',
-         render: (text) => {
-            return checkDiscount(text);
-         }
-      },
-      {
-         title: 'Төлсөн дүн',
-         render: (_, row) => {
-            return numberToCurrency(row.totalAmount);
-         }
-      },
-      {
-         title: 'Ажилтны нэр',
-         dataIndex: 'createdEmployeeName'
-      },
-      {
-         title: 'Харах',
-         width: 50,
-         render: (_, row) => {
-            return (
-               <Button type="link" onClick={() => viewModal(row.id, false)} title="Харах" style={{ paddingRight: 5 }}>
-                  <EyeOutlined />
-               </Button>
-            );
-         }
-      },
-      {
-         title: 'ДАХИН ТАТАХ',
-         width: 50,
-         render: (_, row) => {
-            return (
-               <Button type="link" onClick={() => reload(row.id)} title="ТАТАХ" style={{ paddingRight: 5 }}>
-                  <DownloadOutlined />
-               </Button>
-            );
-         }
-      },
-      {
-         title: 'Буцаалт',
-         render: (_, row) => {
-            return (
-               <Button
-                  type="link"
-                  onClick={() => viewModal(row.id, true)}
-                  title="Харах"
-                  style={{ paddingRight: 5, color: 'red' }}
-               >
-                  <RollbackOutlined />
-               </Button>
-            );
-         }
-      }
-   ];
    useEffect(() => {
-      ScrollRef(scrollRef);
       getDailyIncome(1, 10, today, today);
       getDiscount();
    }, []);
@@ -220,7 +138,7 @@ function DailyIncome() {
                      title="ТТ Огноо"
                      width={120}
                      render={(text, row) => {
-                        if (row?.merchantId != null) {
+                        if (row?.customerNo) {
                            return (
                               <div className="bg-[#5cb85c] text-white">{moment(text).format('YYYY/MM/DD HH:mm')}</div>
                            );
