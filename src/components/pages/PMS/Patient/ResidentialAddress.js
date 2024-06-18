6ed8d4664467eba94cdaf56efa28e8b678cd3f6e
Ulzii786 <72004186+Ulzii786@users.noreply.github.com>
2024-06-18 15:28:05 +0800

    fix

--- src/components/pages/PMS/Patient/ResidentialAddress.js
+++ src/components/pages/PMS/Patient/ResidentialAddress.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { Form, Input } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Form, Input, Select } from 'antd';
+// import districtApi from '@ApiServices/reference/district';
+import provinceApi from '@ApiServices/reference/province';
 
 function ResidentialAddress({ form }) {
+   const [provinces, setProvinces] = useState([]);
+   // const [districts, setDistricts] = useState([]);
+   // const provinceId = Form.useWatch('provinceId', form);
+
+   const getProvinces = () => {
+      provinceApi.get().then((res) => {
+         if (res.data.success) {
+            setProvinces(res.data.response);
+         }
+      });
+   };
+   // const getDistricts = useCallback(() => {
+   //    districtApi.get({ provinceId }).then((res) => {
+   //       if (res.data.success) {
+   //          setDistricts(res.data.response);
+   //       }
+   //    });
+   // }, [provinceId]);
+   useEffect(() => {
+      getProvinces();
+   }, []);
+   // useEffect(() => {
+   //    getDistricts();
+   // }, [getDistricts]);
    return (
       <div className="rounded-md bg-[#F3F4F6] p-2 flex flex-col gap-2">
          <div className="flex flex-row gap-2 items-end">
             <Form.Item
                label="Аймаг/Хот:"
                className="w-full"
-               name={'aimagCityName'}
+               name={'aimagCityCode'}
                rules={[{ required: true, message: 'Аймаг/Хот оруулна уу' }]}
             >
-               <Input />
+               <Select options={provinces.map((item) => ({ label: item.name, value: item.code }))} />
             </Form.Item>
-            <Form.Item label="" name={'aimagCityCode'} hidden>
+            <Form.Item label="" name={'aimagCityName'} hidden>
                <Input disabled className="w-10" />
             </Form.Item>
          </div>
